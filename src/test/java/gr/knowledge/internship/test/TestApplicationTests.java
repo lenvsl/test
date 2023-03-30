67d921e367c55a6f533dc87d4cf6a22d4ff6f540
evasilaki <101207622+lenvsl@users.noreply.github.com>
2023-03-30 10:39:52 +0300

    Initial commit

---
+++ src/test/java/gr/knowledge/internship/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.knowledge.internship.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
